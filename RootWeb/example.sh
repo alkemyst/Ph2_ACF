64f66e7f6b3e1a98ebccbf13196c0b65a2677547
Stefano Mersi <alkemyst@users.noreply.github.com>
2015-06-19 19:21:14 +0200

    Adding a small hint to the user

--- RootWeb/example.sh
+++ RootWeb/example.sh
@@ -5,3 +5,7 @@ echo This should be changed in order to point to a website reachable from afs
 tar -xf style.tar -C /tmp
 
 make && ./example.bin
+
+echo Now try looking at this:
+echo firefox /tmp/mySite/index.html
+
